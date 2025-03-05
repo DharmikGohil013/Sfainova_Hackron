7fa3e4b1285fbce9adf163124e2f0e3ce9b92a8c
DHARMIKgohil013 <dharmikgohil395003@gmail.com>
2025-03-05 14:06:44 +0530

    Add nav bar

--- Frontend/Sfainova/src/Navbar/nav.jsx
+++ Frontend/Sfainova/src/Navbar/nav.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "./nav.css";
+import logo from "../assets/img/bugb.png"; // Adjust the path based on your folder structure
 
 const Nav = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark custom-navbar">
       <div className="container">
-        <a className="navbar-brand" href="#">MyBrand</a>
+        <a className="navbar-brand fw-bold" href="#">
+          <img src={logo} alt="Logo" className="me-2" width="40" height="40" />
+            BugBusters
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -21,6 +26,9 @@ const Nav = () => {
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Contact</a>
+            </li>
+            <li className="nav-item">
+              <a className="btn btn-light rounded-pill ms-3" href="#">Sign Up</a>
             </li>
           </ul>
         </div>
