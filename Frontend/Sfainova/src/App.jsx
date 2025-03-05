9bb63e3f250efecf0992363edd1d5fbbe2dbf433
DHARMIKgohil013 <dharmikgohil395003@gmail.com>
2025-03-05 13:55:39 +0530

    add nav bar

--- Frontend/Sfainova/src/App.jsx
+++ Frontend/Sfainova/src/App.jsx
@@ -3,20 +3,18 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from '../src/Components/AdminLogin';
 import AdminLogin from '../src/Components/Dashboard';
-import Dashboard from '../src/Components/home'; // Placeholder for after login
+import Dashboard from '../src/Components/home'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from "./Navbar/nav";
+// Placeholder for after login
 
 function App() {
   const [userRole, setUserRole] = useState(null); // Track logged-in user role
 
   return (
     <Router>
+       <Nav />
       <div className="app">
-        <nav className="navbar">
-          <Link to="/">Home</Link>
-          {!userRole && <Link to="/admin-login">Admin Login</Link>}
-          {userRole && <Link to="/dashboard">Dashboard</Link>}
-        </nav>
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/admin-login" element={<AdminLogin setUserRole={setUserRole} />} />
