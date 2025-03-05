fad76c4f2931bf3fd8d25b412748cbbb3bc0205d
DHARMIKgohil013 <dharmikgohil395003@gmail.com>
2025-03-05 18:20:44 +0530

    add

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/Sfainova')
+mongoose.connect('mongodb://127.0.0.1:27017/fit-sync')
   .then(() => {
     console.log('MongoDB connected successfully');
   })
--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,36 @@
-const express = require('express');
-const session = require('express-session');
-const body_parser = require('body-parser');
-const cors = require('cors');
+const app = require('./app');
 const db = require('./config/db');
-const fs = require('fs');
-const path = require('path');
-const multer = require('multer');
+const userModel = require('./models/user.model');
+const express = require('express');
+const reportRouter = require('./routers/report.router');
+const bloodReportRoutes = require('./routers/bloodReport.router'); 
+const postRoutes = require('./routers/postRoutes');
+const doctorRoutes = require('./routers/doctorRoutes');
+const stepRoutes = require('./');
+const cors = require('cors');
 
-const app = express();
 const port = 3000;
 
-app.use(session({
-    secret: "your_secret_key",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }
-}));
-
-app.use(body_parser.json());
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+
+// Routes
+app.use('/api/reports', reportRouter);
+app.use('/api/blood-reports', bloodReportRoutes); 
+app.use('/api/posts', postRoutes);
+server.use('/api', stepRoutes);
+server.use('/', doctorRoutes);
+
+const fs = require('fs');
+const path = require('path');
+const uploadDir = path.join(__dirname, 'uploads');
+app.use('/', doctorRoutes);
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 app.get('/', (req, res) => {
     res.send("Hello World");
@@ -29,3 +39,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server listening on port http://localhost:${port}`);
 });
+
