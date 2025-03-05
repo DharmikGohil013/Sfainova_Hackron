704231e619570ffa61864c7f1ce5ae7ee6e581ef
DHARMIKgohil013 <dharmikgohil395003@gmail.com>
2025-03-05 12:18:20 +0530

    Changes in banckend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/fit-sync')
+mongoose.connect('mongodb://127.0.0.1:27017/Sfainova')
   .then(() => {
     console.log('MongoDB connected successfully');
   })
